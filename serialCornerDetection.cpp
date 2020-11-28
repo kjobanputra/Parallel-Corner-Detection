1639dc5564dc70f1e1b2363c61436d6aebd2e822
kjobanputra <kunjoban@gmail.com>
2020-11-28 12:49:16 -0600

    added nonMaxSupression

--- serialCornerDetection.cpp
+++ serialCornerDetection.cpp
@@ -6,7 +6,8 @@
 
 using namespace cv;
 using namespace std;
-Mat src, src_gray;
+const char* window = "window";
+Mat src_gray;
 
 const float gaussianKernel[3][3] = {
     {(1.0 / 16), (1.0 / 8), (1.0 / 16)},
@@ -52,9 +53,9 @@ float c(int i, int j) {
 
   for (int l = i - halfWindow; l <= i + halfWindow; l++) {
     for (int m = j - halfWindow; m <= j + halfWindow; m++) {
-      int partialXVal = partialX(l, m);
-      int partialYVal = partialY(l, m);
-      int gaussianVal = gaussianKernel[l - (i - halfWindow)][m - (j - halfWindow)];
+      float partialXVal = partialX(l, m);
+      float partialYVal = partialY(l, m);
+      float gaussianVal = gaussianKernel[l - (i - halfWindow)][m - (j - halfWindow)];
       gxx += partialXVal*partialXVal*gaussianVal;
       gxy += partialXVal*partialYVal*gaussianVal;
       gyy += partialYVal*partialYVal*gaussianVal;
@@ -66,45 +67,78 @@ float c(int i, int j) {
 
 }
 
-void cornerHarris(Mat harris) {
-  for (int i = 0; i < src_gray.rows; i++) {
-    for (int j = 0; j < src_gray.cols; j++) {
-      harris.at<float>(i, j) = c(i, j);
+void nonMaxSupression(Mat cMatrix, Mat &harris) {
+  for (int i = 0; i < cMatrix.rows; i++) {
+    for (int j = 0; j < cMatrix.cols; j++) {
+      float maxVal = 0;
+      int maxK = -1;
+      int maxL = -1;
+      for (int k = i; k < min(i + convolutionWindowSize, cMatrix.rows); k++) {
+        for (int l = j; l < min(j + convolutionWindowSize, cMatrix.cols); l++) {
+          if (cMatrix.at<float>(k, l) > maxVal) {
+            maxVal = cMatrix.at<float>(k,l);
+            maxK = k;
+            maxL = l;
+          }
+        }
+      }
+
+      if (maxVal > 0) {
+        harris.at<float>(maxK, maxL) = cMatrix.at<float>(maxK, maxL);
+      }
     }
   }
 }
 
+void cornerHarris(Mat &harris) {
+  Mat cMatrix = Mat::zeros(src_gray.size(), CV_32FC1);
+  for (int i = 0; i < src_gray.rows; i++) {
+    for (int j = 0; j < src_gray.cols; j++) {
+      //cout << "c of " << i  << " " << j << " is " << c(i,j) << endl;
+      cMatrix.at<float>(i, j) = c(i, j);
+    }
+  }
+
+  nonMaxSupression(cMatrix, harris);
+}
+
 int main(int argc, char **argv) {
-    if(argc < 2) {
-        printf("Usage: ./harrisCorner image_path\n");
-        exit(-1);
-    }
+  if(argc < 2) {
+    printf("Usage: ./harrisCorner image_path\n");
+    exit(-1);
+  }
 
-    const char *img_path = argv[ARG_IMG];
-    Mat src = imread(img_path);
-    cvtColor(src, src_gray, COLOR_BGR2GRAY);
+  const char *img_path = argv[ARG_IMG];
+  Mat src = imread(img_path);
 
-    if(src_gray.empty()) {
-        printf("Image %s not found\n", img_path);
-        exit(-1);
-    }
+  if (src.empty())
+  {
+    cout << "Image not found: " << img_path << endl;
+    return -1;
+  }
 
-  //cvtColor(src, src_gray, COLOR_BGR2GRAY);
+  cvtColor(src, src_gray, COLOR_BGR2GRAY);
+  if(src_gray.empty()) {
+      printf("Image %s not found\n", img_path);
+      exit(-1);
+  }
+
   Mat harris = Mat::zeros(src.size(), CV_32FC1);
   cornerHarris(harris);
-  Mat harrisNorm, harrisNormScaled;
   for (int i = 0; i < harris.rows; i++) {
     for (int j = 0; j < harris.cols; j++) {
+      cout << "Harris at " << i;
+      cout << " " << j << " is " << harris.at<float>(i,j) << endl;
       if (harris.at<float>(i,j) > thresholdVal) {
         // points out edges that stick out
-        circle(harrisNormScaled, Point(j,i), 3, Scalar(0));
+        circle(src, Point(j,i), 3, Scalar(0));
       }
     }
   }
 
-  //const char* window = "window";
   //namedWindow(window);
-  //imshow(window, harris);
+  //imshow(window, src);
+  //waitKey();
   imwrite("output/callibration.jpg", harris);
   return 0;
 }
